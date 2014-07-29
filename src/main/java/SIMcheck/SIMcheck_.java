09929c0992be7af763e2070c8ca7a32b0589f77e
Graeme Ball <graemeball@googlemail.com>
2014-07-29 23:29:25 +0100

    bump version to 0.9.4

--- src/main/java/SIMcheck/SIMcheck_.java
+++ src/main/java/SIMcheck/SIMcheck_.java
@@ -81,7 +81,7 @@ public class SIMcheck_ implements PlugIn {
             IJ.noImage();
             return;
         }
-        GenericDialog gd = new GenericDialog("SIMcheck (v0.9.3)");
+        GenericDialog gd = new GenericDialog("SIMcheck (v0.9.4)");
         gd.addMessage(
                 "--------------- INSTRUCTIONS ---------------");
         gd.addMessage(
